8ae7ed108634b875ed89991cfb2a2f63928f9fd9
Anurag Panda <anuragpanda.24@nshm.edu.in>
2026-03-25 11:40:21 +0530

    Implement random commit generation function
    Refactor commit logic and add random commit generation.

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 import jsonfile from 'jsonfile';
 import moment from 'moment';
 import simpleGit from 'simple-git';
+import random from "random";
 
 const root = './';
 const path = './data.json';
-//const date = moment().subtract(1, 'day').format();
+//const date = moment().subtract(1, 'day').format(); make commit yesterday
 
+/* add to a specific location (x -> row, y -> column)
 const markCommit = (x, y) => {
     const date = moment()
     .subtract(1, "y")
@@ -14,7 +16,9 @@ const markCommit = (x, y) => {
     .add(y, "d")
     .format();
 }
+*/
 
+/* for upper two methods
 const data = {
     date: date,
 }
@@ -22,4 +26,22 @@ const data = {
 jsonfile.writeFile(path, data, ()=>{
     simpleGit().add([root]).commit(date, { '--author': 'Anurag Panda', '--date': date }).push();
 });
+*/
+
+const makeCommits = (n) => {
+    if (n==0) return simpleGit.push();
+    const x = random.default.int(0, 54);
+    const y = random.default.int(0, 6);
+    const date = moment().subtract(1, "y").add(1, "d").add(x, "w").add(y, "d").format();
+
+    const data = {
+        date: date,
+    }
+
+    jsonfile.writefile(path, data, () => {
+        simpleGit().add([path]).commit(date, { "--date": date }, makeCommits.bind(this,--n); //or remove makeCommits.bind(this, --n) and add .push()
+    });
+};
+
+makeCommits(100); //usage
 
