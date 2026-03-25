d23d277e7fdf49f998629257917ea70c60df07ba
Anurag Panda <anuragpanda.24@nshm.edu.in>
2026-03-25 11:50:16 +0530

    Refactor commit-making logic in index.js

--- index.js
+++ index.js
@@ -2,7 +2,31 @@ import jsonfile from 'jsonfile';
 import moment from 'moment';
 import simpleGit from 'simple-git';
 
+const root = './';
 const path = './data.json';
+
+//const date = moment().subtract(1, 'day').format(); make commit yesterday
+
+/* add to a specific location (x -> row, y -> column)
+const markCommit = (x, y) => {
+    const date = moment()
+    .subtract(1, "y")
+    .add(1, "d")
+    .add(x, "w")
+    .add(y, "d")
+    .format();
+}
+*/
+
+/* for upper two methods
+const data = {
+    date: date,
+}
+
+jsonfile.writeFile(path, data, ()=>{
+    simpleGit().add([root]).commit(date, { '--author': 'Anurag Panda', '--date': date }).push();
+});
+*/
 
 const makeCommits = (n) => {
     if (n <= 0) return simpleGit().push();
@@ -15,7 +39,7 @@ const makeCommits = (n) => {
     };
 
     jsonfile.writeFile(path, data, () => {
-        simpleGit().add([path]).commit(date, { "--date": date }, makeCommits.bind(this, n - 1));
+        simpleGit().add([path]).commit(date, { "--date": date }, makeCommits.bind(this, n - 1)); //or remove makeCommits.bind(this, n-1) and add .push()
     });
 };
 
